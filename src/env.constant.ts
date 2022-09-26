efb77a9d8a9ca66c258118e13deceeb1d86cf052
Mykhaylo Gerasika <mgerasika@gmail.com>
2022-09-26 08:10:46 +0300

    1

--- src/env.constant.ts
+++ src/env.constant.ts
@@ -1,8 +1,7 @@
 require("dotenv").config();
 export const ENV = {
-  VIBER_PROXY_SERVER_URL: process.env.VIBER_PROXY_SERVER_URL,
+  VIBER_PROXY_SERVER_URL: "https://parus-smart-bot.herokuapp.com/",
+  DEBUG_VIBER_SERVER_URL: "http://178.210.131.101:1337/api/viber/web_hook",
   VIBER_PROXY_TOKEN: process.env.VIBER_PROXY_TOKEN,
-  DEBUG_VIBER_SERVER_URL: process.env.DEBUG_VIBER_SERVER_URL,
 };
 console.log("ENV=", ENV);
-// console.log("Buffer = ", Buffer.from(ENV.GOOGLE_API_CRED).toString("base64"));
